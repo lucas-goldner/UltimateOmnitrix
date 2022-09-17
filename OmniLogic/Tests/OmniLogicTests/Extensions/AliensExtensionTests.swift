5cd5d2b81f803d94d74bb594fcedc8b031a1f8ee
lucas-goldner <lucas.goldner@googlemail.com>
2022-09-17 23:10:31 +0200

    test: Fix current tests

--- OmniLogic/Tests/OmniLogicTests/Extensions/AliensExtensionTests.swift
+++ OmniLogic/Tests/OmniLogicTests/Extensions/AliensExtensionTests.swift
@@ -2,7 +2,7 @@
 import XCTest
 
 final class AliensExtensionTests: XCTestCase {
-    func testGetCorrectImageKey() throws {
+    func testOGAliensImageKey() throws {
         XCTAssertEqual(OGAliens.ghostfreak.imageKey, "Ghostfreak")
         XCTAssertEqual(OGAliens.diamondhead.imageKey, "Diamondhead")
         XCTAssertEqual(OGAliens.fireblast.imageKey, "Fireblast")
@@ -13,5 +13,25 @@ final class AliensExtensionTests: XCTestCase {
         XCTAssertEqual(OGAliens.xlr8.imageKey, "XLR8")
         XCTAssertEqual(OGAliens.upgrade.imageKey, "Upgrade")
         XCTAssertEqual(OGAliens.wildmutt.imageKey, "Wildmutt")
+    }
+
+    func testOGAliensPlusImageKey() throws {
+        XCTAssertEqual(OGAliensPlus.ghostfreak.imageKey, "Ghostfreak")
+        XCTAssertEqual(OGAliensPlus.diamondhead.imageKey, "Diamondhead")
+        XCTAssertEqual(OGAliensPlus.fireblast.imageKey, "Fireblast")
+        XCTAssertEqual(OGAliensPlus.fourarms.imageKey, "Fourarms")
+        XCTAssertEqual(OGAliensPlus.graymatter.imageKey, "Graymatter")
+        XCTAssertEqual(OGAliensPlus.ripjaws.imageKey, "Ripjaws")
+        XCTAssertEqual(OGAliensPlus.stinkfly.imageKey, "Stinkfly")
+        XCTAssertEqual(OGAliensPlus.xlr8.imageKey, "XLR8")
+        XCTAssertEqual(OGAliensPlus.upgrade.imageKey, "Upgrade")
+        XCTAssertEqual(OGAliensPlus.wildmutt.imageKey, "Wildmutt")
+        XCTAssertEqual(OGAliensPlus.wildvine.imageKey, "Wildvine")
+        XCTAssertEqual(OGAliensPlus.ditto.imageKey, "Ditto")
+        XCTAssertEqual(OGAliensPlus.upchuck.imageKey, "Upchuck")
+        XCTAssertEqual(OGAliensPlus.waybig.imageKey, "Waybig")
+        XCTAssertEqual(OGAliensPlus.frankenstrike.imageKey, "Frankenstrike")
+        XCTAssertEqual(OGAliensPlus.snareoh.imageKey, "Snareoh")
+        XCTAssertEqual(OGAliensPlus.blitzwolfer.imageKey, "Blitzwolfer")
     }
 }
